ce4f3b05ce380fb2e66e8db1a16d6a261cc11d24
Wildbook <book.wille@gmail.com>
2022-01-05 15:30:58 +0100

    Avoid unnecessary string allocations

--- src/main.rs
+++ src/main.rs
@@ -60,25 +60,25 @@ pub fn find_pattern<'a>(data: &'a [u8], pattern: &str) -> Vec<(usize, &'a [u8])>
         .collect()
 }
 
-pub fn get_processor(ver1: u8, ver2: u8) -> Option<String> {
+pub fn get_processor(ver1: u8, ver2: u8) -> Option<&'static str> {
     match [ver1, ver2] {
-        [0x00, 0x38] => Some("Vermeer".to_string()),        // Ryzen 5XXX
-        [0x00, 0x2E] => Some("Matisse".to_string()),        // Ryzen 3XXX
-        [0x00, 0x2B] => Some("Pinnacle Ridge".to_string()), // Ryzen 2XXX
-        [0x00, 0x19] => Some("Summit Ridge".to_string()),   // Ryzen 1XXX
+        [0x00, 0x38] => Some("Vermeer"),        // Ryzen 5XXX
+        [0x00, 0x2E] => Some("Matisse"),        // Ryzen 3XXX
+        [0x00, 0x2B] => Some("Pinnacle Ridge"), // Ryzen 2XXX
+        [0x00, 0x19] => Some("Summit Ridge"),   // Ryzen 1XXX
 
-        [0x00, 0x40] => Some("Cezanne".to_string()),        // Ryzen 5XXX (APU)
-        [0x00, 0x37] => Some("Renoir".to_string()),         // Ryzen 4XXX (APU)
-        [0x04, 0x1E] => Some("Picasso".to_string()),        // Ryzen 3XXX (APU)
-        [0x00, 0x25] => Some("Raven Ridge 2".to_string()),  // Ryzen 2XXX (APU - Refresh)
-        [0x00, 0x1E] => Some("Raven Ridge".to_string()),    // Ryzen 2XXX (APU)
+        [0x00, 0x40] => Some("Cezanne"),        // Ryzen 5XXX (APU)
+        [0x00, 0x37] => Some("Renoir"),         // Ryzen 4XXX (APU)
+        [0x04, 0x1E] => Some("Picasso"),        // Ryzen 3XXX (APU)
+        [0x00, 0x25] => Some("Raven Ridge 2"),  // Ryzen 2XXX (APU - Refresh)
+        [0x00, 0x1E] => Some("Raven Ridge"),    // Ryzen 2XXX (APU)
         
-        [0x04, 0x24] => Some("Castle Peak".to_string()),    // Threadripper 3XXX
-        [0x04, 0x2B] => Some("Colfax".to_string()),         // Threadripper 2XXX
-        [0x04, 0x19] => Some("Whitehaven".to_string()),     // Threadripper 1XXX (also matches Naples - EPYC 7001)
+        [0x04, 0x24] => Some("Castle Peak"),    // Threadripper 3XXX
+        [0x04, 0x2B] => Some("Colfax"),         // Threadripper 2XXX
+        [0x04, 0x19] => Some("Whitehaven"),     // Threadripper 1XXX (also matches Naples - EPYC 7001)
 
-        [0x00, 0x24] => Some("Rome".to_string()),           // EPYC 7003
-        [0x00, 0x2D] => Some("Milan".to_string()),          // EPYC 7002
+        [0x00, 0x24] => Some("Rome"),           // EPYC 7003
+        [0x00, 0x2D] => Some("Milan"),          // EPYC 7002
         _ => None,
     }
 }
@@ -145,7 +145,7 @@ pub fn parse_directory(data: &[u8], address: usize, offset: usize, smus: &mut Ve
                             };
 
                             let processor = get_processor(version[0x3], version[0x2])
-                                .unwrap_or("Unknown".to_string());
+                                .unwrap_or("Unknown");
 
                             log::info!(
                                 "Location {:08X}, Size {:08X} ({:<3} KB) // {} {}",
