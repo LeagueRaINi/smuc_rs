165f3a1b6c787a1f59530b4d8428fb5a73f4214c
LeagueRaINi <razor13372k@gmail.com>
2022-01-08 19:56:47 +0100

    feat: major cleanup
    Signed-off-by: LeagueRaINi <razor13372k@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,39 @@ use std::{env, fs};
 use bytemuck::{try_from_bytes, Pod, Zeroable};
 use regex::bytes::Regex;
 
+macro_rules! make_dir {
+    ($visibility:vis $name:ident, $header_type:ty, $entry_type:ty) => {
+        #[allow(dead_code)]
+        $visibility struct $name<'a> {
+            address: usize,
+            header: &'a $header_type,
+            entries: Vec<&'a $entry_type>,
+        }
+
+        impl $name<'_> {
+            pub fn new(address: usize, data: &[u8]) -> Option<$name> {
+                let data = &data[address..];
+
+                const HEADER_SIZE: usize = size_of::<$header_type>();
+                const ENTRY_SIZE: usize = size_of::<$entry_type>();
+
+                try_from_bytes::<$header_type>(&data[..HEADER_SIZE])
+                    .and_then(|header| {
+                        Ok($name {
+                            address,
+                            header,
+                            entries: data[HEADER_SIZE..][..header.entries as usize * ENTRY_SIZE]
+                                .chunks_exact(ENTRY_SIZE)
+                                .filter_map(|chunk| try_from_bytes::<$entry_type>(chunk).ok())
+                                .collect::<Vec<_>>(),
+                        })
+                    })
+                    .ok()
+            }
+        }
+    }
+}
+
 #[derive(Debug, Copy, Clone, Pod, Zeroable)]
 #[repr(C)]
 struct FirmwareEntryTable {
@@ -36,7 +69,9 @@ struct ComboDirectoryHeader {
 #[repr(C)]
 struct PspDirectoryEntry {
     pub kind: u8,
-    pub rsvd_02: [u8; 0x3],
+    pub sub_program: u8,
+    pub rom_id: u8,
+    pub rsvd_03: u8,
     pub size: u32,
     pub location: u64,
 }
@@ -49,6 +84,9 @@ struct ComboDirectoryEntry {
     pub location: u64,
 }
 
+make_dir!(ComboDirectory, ComboDirectoryHeader, ComboDirectoryEntry);
+make_dir!(PspDirectory, DirectoryHeader, PspDirectoryEntry);
+
 pub fn find_pattern<'a>(data: &'a [u8], pattern: &str) -> Vec<(usize, &'a [u8])> {
     let regex_string = &["(?s-u)", pattern].concat();
     let regex = Regex::new(regex_string).expect("Invalid regex");
@@ -60,111 +98,96 @@ pub fn find_pattern<'a>(data: &'a [u8], pattern: &str) -> Vec<(usize, &'a [u8])>
         .collect()
 }
 
-pub fn get_processor(ver1: u8, ver2: u8) -> Option<&'static str> {
+pub fn get_processor_arch(ver1: u8, ver2: u8) -> Option<&'static str> {
     match [ver1, ver2] {
         [0x00, 0x38] => Some("Vermeer"),        // Ryzen 5XXX
         [0x00, 0x2E] => Some("Matisse"),        // Ryzen 3XXX
         [0x00, 0x2B] => Some("Pinnacle Ridge"), // Ryzen 2XXX
         [0x00, 0x19] => Some("Summit Ridge"),   // Ryzen 1XXX
 
-        [0x00, 0x40] => Some("Cezanne"),        // Ryzen 5XXX (APU)
-        [0x00, 0x37] => Some("Renoir"),         // Ryzen 4XXX (APU)
-        [0x04, 0x1E] => Some("Picasso"),        // Ryzen 3XXX (APU)
-        [0x00, 0x25] => Some("Raven Ridge 2"),  // Ryzen 2XXX (APU - Refresh)
-        [0x00, 0x1E] => Some("Raven Ridge"),    // Ryzen 2XXX (APU)
+        [0x00, 0x40] => Some("Cezanne"),       // Ryzen 5XXX (APU)
+        [0x00, 0x37] => Some("Renoir"),        // Ryzen 4XXX (APU)
+        [0x04, 0x1E] => Some("Picasso"),       // Ryzen 3XXX (APU)
+        [0x00, 0x25] => Some("Raven Ridge 2"), // Ryzen 2XXX (APU - Refresh)
+        [0x00, 0x1E] => Some("Raven Ridge"),   // Ryzen 2XXX (APU)
 
-        [0x04, 0x24] => Some("Castle Peak"),    // Threadripper 3XXX
-        [0x04, 0x2B] => Some("Colfax"),         // Threadripper 2XXX
-        [0x04, 0x19] => Some("Whitehaven"),     // Threadripper 1XXX (also matches Naples - EPYC 7001)
+        [0x04, 0x24] => Some("Castle Peak"), // Threadripper 3XXX
+        [0x04, 0x2B] => Some("Colfax"),      // Threadripper 2XXX
+        [0x04, 0x19] => Some("Whitehaven"),  // Threadripper 1XXX (also matches Naples - EPYC 7001)
 
-        [0x00, 0x24] => Some("Rome"),           // EPYC 7003
-        [0x00, 0x2D] => Some("Milan"),          // EPYC 7002
+        [0x00, 0x24] => Some("Rome"),  // EPYC 7003
+        [0x00, 0x2D] => Some("Milan"), // EPYC 7002
         _ => None,
     }
 }
 
 pub fn parse_directory(data: &[u8], address: usize, offset: usize, smus: &mut Vec<usize>) {
-    let calc_address = |address: usize| (address & 0x00FFFFFF) + offset;
+    let convert_address = |address: usize| (address & 0x00FFFFFF) + offset;
 
-    let address = calc_address(address);
+    let address = convert_address(address);
 
     match &data[address..][..4] {
         b"2PSP" => {
-            let header = match try_from_bytes::<ComboDirectoryHeader>(
-                &data[address..][..size_of::<ComboDirectoryHeader>()],
-            ) {
-                Ok(header) => header,
-                _ => {
-                    log::error!("Failed to parse 2PSP at {:08X}", address);
+            let directory = match ComboDirectory::new(address, data) {
+                Some(directory) => directory,
+                None => {
+                    log::error!("Failed to parse combo directory at {:08X}", address);
                     return;
                 },
             };
 
-            let entries = &data[address + size_of::<ComboDirectoryHeader>()..]
-                [..size_of::<ComboDirectoryEntry>() * header.entries as usize];
-
-            entries
-                .chunks_exact(size_of::<ComboDirectoryEntry>())
-                .filter_map(|x| try_from_bytes::<ComboDirectoryEntry>(x).ok())
-                .for_each(|entry| parse_directory(data, entry.location as usize, offset, smus));
+            directory
+                .entries
+                .iter()
+                .for_each(|entry| parse_directory(data, entry.location as usize, offset, smus))
         },
         b"$PSP" | b"$PL2" => {
-            let header = match try_from_bytes::<DirectoryHeader>(
-                &data[address..][..size_of::<DirectoryHeader>()],
-            ) {
-                Ok(header) => header,
-                _ => {
-                    log::error!("Failed to parse $PSP/$PL2 at {:08X}", address);
+            let directory = match PspDirectory::new(address, data) {
+                Some(directory) => directory,
+                None => {
+                    log::error!("Failed to parse psp directory at {:08X}", address);
                     return;
                 },
             };
 
-            let entries = &data[address + size_of::<DirectoryHeader>()..]
-                [..size_of::<PspDirectoryEntry>() * header.entries as usize];
+            directory.entries.iter().for_each(|entry| {
+                match entry.kind {
+                    // SMU Firmware
+                    0x08 | 0x12 => {
+                        let location = convert_address(entry.location as usize);
 
-            entries
-                .chunks_exact(size_of::<PspDirectoryEntry>())
-                .filter_map(|x| try_from_bytes::<PspDirectoryEntry>(x).ok())
-                .for_each(|entry| {
-                    match entry.kind {
-                        // SMU Firmware
-                        0x08 | 0x12 => {
-                            let location = calc_address(entry.location as usize);
+                        if smus.contains(&location) {
+                            return;
+                        }
 
-                            if smus.contains(&location) {
-                                return;
+                        let version = {
+                            let bytes = &data[location + 0x60..][..4];
+                            if bytes == [0x00, 0x00, 0x00, 0x00] {
+                                &data[location..][..0x4]
+                            } else {
+                                bytes
                             }
+                        };
 
-                            let version = {
-                                let bytes = &data[location + 0x60..][..4];
-                                if bytes == [0x00, 0x00, 0x00, 0x00] {
-                                    &data[location..][..0x4]
-                                } else {
-                                    bytes
-                                }
-                            };
+                        log::info!(
+                            "Location {:08X}, Size {:08X} ({:<3} KB) // {} {}",
+                            location,
+                            entry.size,
+                            entry.size / 1024,
+                            format!(
+                                "{:02}.{:02}.{:02}.{:02} ({0:02X}.{1:02X}.{2:02X}.{3:02X})",
+                                version[0x3], version[0x2], version[0x1], version[0x00]
+                            ),
+                            get_processor_arch(version[0x3], version[0x2]).unwrap_or("Unknown"),
+                        );
 
-                            let processor = get_processor(version[0x3], version[0x2])
-                                .unwrap_or("Unknown");
-
-                            log::info!(
-                                "Location {:08X}, Size {:08X} ({:<3} KB) // {} {}",
-                                location,
-                                entry.size,
-                                entry.size / 1024,
-                                format!("{:02}.{:02}.{:02}.{:02} ({:02X}.{:02X}.{:02X}.{:02X})",
-                                    version[0x3], version[0x2], version[0x1], version[0x00],
-                                    version[0x3], version[0x2], version[0x1], version[0x00]),
-                                processor,
-                            );
-
-                            smus.push(location);
-                        },
-                        // PSP Level 2 Directory
-                        0x40 | 0x70 => parse_directory(data, entry.location as usize, offset, smus),
-                        _ => (),
-                    }
-                });
+                        smus.push(location);
+                    },
+                    // PSP Level 2 Directory
+                    0x40 | 0x70 => parse_directory(data, entry.location as usize, offset, smus),
+                    _ => (),
+                }
+            });
         },
         _ => (),
     }
@@ -182,7 +205,7 @@ fn main() {
 
     let data = fs::read(path).unwrap();
 
-    log::info!("BIOS: {}", file_name);
+    log::info!("BIOS: {} ({} KB)", file_name, data.len() / 1024);
 
     let agesa = find_pattern(&data, r"(AGESA![0-9a-zA-Z]{0,10}\x00{0,1}[0-9a-zA-Z .\-]+)")
         .into_iter()
